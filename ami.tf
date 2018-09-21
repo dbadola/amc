8b43faa8d085baf1615c1d0f710ad6f9cc41e038
dbadola <36331069+dbadola@users.noreply.github.com>
2018-09-21 14:55:32 +0800

    Create ami.tf

---
+++ ami.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-west-2"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+output "ami_id {
+  value = "${aws_ami.ubuntu.name}"
+}
